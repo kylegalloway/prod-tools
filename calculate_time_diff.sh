736efb5cc3050b9b34b1b52b921df2a665135b56
Kyle Galloway <kyle.galloway.1993@gmail.com>
2019-04-17 11:10:16 -0500

    feature: add more usages and add help

--- calculate_time_diff.sh
+++ calculate_time_diff.sh
@@ -16,8 +16,17 @@ calculate_total() {
     echo "Total Time: $(convertsecs total)"
 }
 
+print_usage() {
+	echo -e "Pass 1 argument for the time difference between that argument and now.\n"
+	echo -e "\t ./calculate_time_diff.sh 09:45 \n"
+	echo -e "Pass 2 arguments for the time difference between the 1st and 2nd argument.\n"
+	echo -e "\t ./calculate_time_diff.sh 09:45 10:23\n"
+}
+
 if [ "$#" -eq 1 ]; then
     calculate_total $1 $(TZ=America/Chicago date | grep -o '[0-2][0-9]:[0-5][0-9]')
-else
+elif [ "$#" -eq 2 ]; then
     calculate_total $1 $2
+else
+    print_usage
 fi
--- clock.sh
+++ clock.sh
@@ -2,4 +2,17 @@
 
 filename="timecard.log"
 
-echo "[$(TZ=America/Chicago date)] --- ${1}" >> "${filename}"
+print_usage() {
+	echo -e "Pass 1 argument to log an activity for the current time to ${filename}.\n"
+	echo -e "\t ./clock.sh \"Project X\"\n"
+	echo -e "Pass 2 arguments to log an activity for a time offset to ${filename}.\n"
+	echo -e "\t ./clock.sh \"Project X\" \"-2days-3hours-24min-41sec\"\n"
+}
+
+if [ "$#" -eq 1 ]; then
+    echo "[$(TZ=America/Chicago date)] --- ${1}" >> "${filename}"
+elif [ "$#" -eq 2 ]; then
+    echo "[$(TZ=America/Chicago date -d ${2})] --- ${1}" >> "${filename}"
+else
+    print_usage
+fi
